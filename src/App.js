9f33394e960aa505d1f023b9421e6d91189b4648
basedHokage <chriseugene75@gmail.com>
2020-09-16 03:03:38 +0300

    finished app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [backgroundColor, setBackgroundColor] = useState("black");
+  const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState("");
+
+  const setStyle = () => {
+    if(inputValue) {
+    setBackgroundColor(inputValue);
+    } else {
+      setError("Please enter a colour")
+    }
+  };
+  const onChange = (e) => {
+    setInputValue(e.target.value)
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="form__container">
+        <div
+          className="circle"
+          style={{backgroundColor}}
+        ></div>
+        <form className="form">
+          <div className="form__item">
+            <label className="form__label" htmlFor="color">Enter A Colour for the Circle</label>
+            <input
+              className="form__input"
+              type="text"
+              name="inputValue"
+              value={inputValue}
+              required
+              placeholder="Enter a colour"
+              onChange={onChange}
+            ></input>
+            {error && <div className="form__error">
+              {error}</div>}
+          </div>
+          <div className="form__item">
+            <button className="form__btn" onClick={setStyle}>
+              Set Colour
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
-
 export default App;
